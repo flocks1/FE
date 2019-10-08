33c3a0feb8355309bbf50cb2d0351a23d055eaec
dgbailey <bailey.dust@gmail.com>
2019-10-08 11:59:06 -0700

    more notes on thunks, redux middleware, composition, inversion of control, partial application and currying, action creators

--- src/actions/__tests__/loginAction.spec.js
+++ src/actions/__tests__/loginAction.spec.js
@@ -3,25 +3,40 @@ import thunk from 'redux-thunk';
 import {initiateLogin} from '../../actions/login';
 import axiosMock from 'axios';
 import {LOGIN_START, LOGIN_SUCCESS} from '../../actions/login';
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
 
-it('should fetch mock data',() =>{
-    const store = mockStore({});
 
-    axiosMock.post.mockResolvedValueOnce({
-        data: { payload: 'test-complete' },
+
+describe('Tests login redux actions',()=>{
+
+    
+    const middlewares = [thunk];
+    const mockStore = configureMockStore(middlewares);
+    
+
+    it('should return dispatched login start and login success actions',() =>{
+        
+        const store = mockStore({});
+        //the mock store will create an array of dispatched actions which serve as an action log for tests.
+        
+
+       
+
+
+        //jest mocking npm module axios
+        axiosMock.post.mockResolvedValueOnce({
+            data: { payload: 'test-complete' },
+            })
+    
+    
+        const expectedActions = [{type:LOGIN_START},{type:LOGIN_SUCCESS,payload:'test-complete'}];
+    
+        const creds = {'username':'test','password':'test'};
+    
+    
+        return store.dispatch(initiateLogin(creds)).then(() =>{
+            expect(store.getActions()).toEqual(expectedActions);
         })
-
-
-    const expectedActions = [{type:LOGIN_START},{type:LOGIN_SUCCESS,payload:'test-complete'}];
-
-    const creds = {'username':'test','password':'test'};
-
-
-    return store.dispatch(initiateLogin(creds)).then(() =>{
-        expect(store.getActions()).toEqual(expectedActions);
+    
+    
     })
-
-
 })
